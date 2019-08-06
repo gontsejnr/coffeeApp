966b5b42410e5d527c66ade58a1e84365aed52ef
gontsejnr <gontsemaepa@gmail.com>
2019-08-06 11:28:24 +0200

    last

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'order', pathMatch: 'full' },
+  { path: '', redirectTo: 'coffee', pathMatch: 'full' },
   { path: 'order', loadChildren: './page/order/order.module#OrderPageModule' },
   { path: 'confirm', loadChildren: './page/confirm/confirm.module#ConfirmPageModule' },
-  { path: 'coffee', loadChildren: './page/coffee/coffee.module#CoffeePageModule' },  { path: 'payment', loadChildren: './page/payment/payment.module#PaymentPageModule' },
+  { path: 'coffee', loadChildren: './page/coffee/coffee.module#CoffeePageModule' },
+  { path: 'payment', loadChildren: './page/payment/payment.module#PaymentPageModule' },
 
 ];
 
